24737e6ca110fa2c8d0c3cf5b33a2b7e15709eb2
Keren Gabriely <keren.umbuzeiro@tivit.com>
2022-12-05 13:10:00 -0300

    Seção 3: Introdução ao EF Core

--- Back/src/ProEventos.API/Program.cs
+++ Back/src/ProEventos.API/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string mysqlConnection = builder.Configuration.GetConnectionString("mysqlConnection");
+
+builder.Services.AddDbContext<DataContext>(options => options.UseMySql(mysqlConnection, ServerVersion.AutoDetect(mysqlConnection)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
