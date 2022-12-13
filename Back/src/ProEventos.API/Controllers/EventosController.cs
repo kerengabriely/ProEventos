951ac3156327fddfc481ab43dc3e8bdd4310a8fc
Keren Gabriely <keren.umbuzeiro@tivit.com>
2022-12-13 13:32:29 -0300

    Seção 4: Angular

---
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventosController : ControllerBase
+{
+
+        private readonly DataContext _context;
+
+    public EventosController(DataContext context)
+    {
+            this._context = context;
+    }
+
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+        return _context.Eventos;
+    }
+
+
+    [HttpGet("{id}")]
+    public Evento GetById(int id)
+    {
+        return _context.Eventos.FirstOrDefault(
+            evento => evento.EventoId == id);
+    }
+
+}
--- Back/src/ProEventos.API/Program.cs
+++ Back/src/ProEventos.API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<DataContext>(options => options.UseMySql(mysqlConn
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,6 +24,10 @@ if (app.Environment.IsDevelopment())
 }  else {
     app.UseHttpsRedirection();
 }
+
+app.UseCors(x => x.AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowAnyOrigin());
 
 app.UseAuthorization();
 
